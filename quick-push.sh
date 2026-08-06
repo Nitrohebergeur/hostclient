a0c0878f2e297ade11ee37c2a663bab5fd804337
neyrix_mc <calix.imbert@gmail.com>
2026-08-06 20:23:33 +0200

    Delete quick-push.sh

--- quick-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-git add install.sh
-git commit -m "fix: Ignore composer post-install errors and continue installation"
-git push origin main
-echo "Pushed! Wait 30 seconds then run install script"
